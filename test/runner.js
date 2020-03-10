45f6389517d99e3a184558826cf515c7b3b30672
saintedlama <christoph.walcher@gmail.com>
2020-03-10 07:42:39 +0100

    chore: add log indirection to test reporters and increase code coverage

--- test/runner.js
+++ test/runner.js
@@ -1,18 +1,85 @@
 const Runner = require('../lib/runner');
 const reporters = require('../lib/reporters');
+const log = require('../lib/reporters/log');
 const { expect } = require('chai');
 
 describe('runner', () => {
+  let result = '';
+
+  beforeEach(() => {
+    result = '';
+    log.backend = function(output) {
+      result += output;
+    }
+  });
+
+  afterEach(() => {
+    log.backend = console.log;
+  });
+
   it('should create new instances with undefined options specified', () => {
     const runner = new Runner();
 
     expect(runner).to.exist;
   });
 
-  it('should execute', async () => {
+  it('should build a report containing package dependencies', async () => {
     const runner = new Runner();
     const reporter = reporters.create('junit');
 
     await runner.execute(reporter);
+
+    expect(result).to.contain('license-checker');
+    expect(result).to.not.contain('<error>');
+  });
+
+  it('should check only development dependencies if specified in options', async () => {
+    const runner = new Runner({ development: true, production: false });
+    const reporter = reporters.create('junit');
+
+    await runner.execute(reporter);
+
+    expect(result).to.not.contain('license-checker');
+    expect(result).to.contain('mocha');
+    expect(result).to.not.contain('<error>');
+  });
+
+  it('should check only production dependencies if specified in options', async () => {
+    const runner = new Runner({ development: false, production: true });
+    const reporter = reporters.create('junit');
+
+    await runner.execute(reporter);
+
+    expect(result).to.contain('license-checker');
+    expect(result).to.not.contain('mocha');
+    expect(result).to.not.contain('<error>');
+  });
+
+  it('should add failed tests if license is blacklisted', async () => {
+    const runner = new Runner({ blacklist: 'MIT' });
+    const reporter = reporters.create('junit');
+
+    await runner.execute(reporter);
+
+    expect(result).to.contain('license-checker');
+    expect(result).to.contain('mocha');
+    expect(result).to.contain('<error>');
+  });
+
+  it('should add ignore results for ignored tests', async () => {
+    const runner = new Runner({ ignore: 'mocha*' });
+    const reporter = reporters.create('junit');
+
+    await runner.execute(reporter);
+
+    expect(result).to.contain('license-checker');
+    expect(result).to.contain('mocha');
+    expect(result).to.contain('<skipped/>');
+  });
+
+  it('should throw if invalid licenses are blacklisted', async () => {
+    expect(() => {
+      new Runner({ blacklist: 'MITTAG' });
+    }).to.throw('Licenses "MITTAG" were not specified in a valid SDPX format');
   });
 });
